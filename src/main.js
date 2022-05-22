928905538d9fce9da65243ef5c68cb3103f07608
Sam Haycraft <phreakdawg@gmail.com>
2022-05-22 01:14:04 -0600

    feat: fixes up classes and logic for printing

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
 const prompt = require("prompt-sync")({ sigInt: true });
-const printFunctionForm = require("./print-function-form");
+const FunctionTerm = require("./function-term");
+const VariableTerm = require("./variable-term");
 
 function init() {
-  const n = prompt("Enter number of variables: ");
+  /**
+   *
+   * @type {string[]}
+   */
+  const variableAliases = [];
 
-  console.log("You got " + n + " variables fool!");
-  console.log(printFunctionForm(+n));
+  /**
+   *
+   * @type {FunctionTerm[]}
+   */
+  const functionTermList = [];
+
+  for (let i of Array(+prompt("Enter number of variables: ")).keys()) {
+    variableAliases[i] = prompt(`Alias for variable #${i + 1}: `);
+  }
+
+  const n = +prompt("Enter number of terms: ");
+
+  for (let i of Array(n).keys()) {
+    console.log(`-------- Term #${i}-------`);
+    const coefficient = +prompt(`Coefficient C${i}: `);
+    /**
+     *
+     * @type {VariableTerm[]}
+     */
+    const variableTermList = [];
+    for (let j of variableAliases.keys()) {
+      const exponent = +prompt(`Enter exponent for ${variableAliases[j]}: `);
+      variableTermList.push(new VariableTerm(variableAliases[j], exponent));
+    }
+
+    functionTermList.push(new FunctionTerm(variableTermList, coefficient));
+    console.debug(functionTermList[i].print());
+  }
 }
 
 init();
---
+++ src/variable-term.js
@@ -0,0 +1,8 @@
+class VariableTerm {
+  constructor(name, exponent) {
+    this.name = name;
+    this.exponent = exponent;
+  }
+}
+
+module.exports = VariableTerm;
